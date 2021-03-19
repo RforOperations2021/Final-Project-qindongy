6d4e1c46a883ed8255b052ad12ce43e48e25a520
qindongy <78916435+qindongy@users.noreply.github.com>
2021-03-18 23:14:07 -0400

    Work in Progress-almost done with one bug

---
+++ Final_qindongy.R
@@ -0,0 +1,171 @@
+library(flexdashboard)
+library(readr)
+library(shiny)
+library(ggplot2)
+library(data.table)
+library(DT)
+library(flexdashboard)
+library(dplyr)
+library(rgdal)
+library(leaflet)
+library(leaflet.extras)
+library(shinythemes)
+library(plotly)
+
+
+data<-read.csv("311data.csv")
+police_zone<-readOGR("https://services1.arcgis.com/YZCmUqbcsUpOKfj7/arcgis/rest/services/PGHWebPoliceZones/FeatureServer/0/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=&returnGeometry=true&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pgeojson&token=")
+data$date<-as.Date(data$date)
+data$weekday<-weekdays(data$date)
+
+data_agg<-data
+setDT(data_agg)
+data_agg=data_agg[,c("REQUEST_ORIGIN","weekday")]
+data_agg=data_agg[,.N,by=.(weekday,REQUEST_ORIGIN)]
+ui <- navbarPage("Pittsburgh Emergency Request Analysis",
+                 theme = shinytheme("united"),
+                 tabPanel("Map",
+                          sidebarLayout(
+                            sidebarPanel(
+                              # Select Sewer Type
+                              radioButtons(
+                                inputId = "Type",
+                                label="Type of information to display in the GIS graph",
+                                choices = c("POLICE_ZONE", "REQUEST_ORIGIN", "GEO_ACCURACY"),
+                                selected = "REQUEST_ORIGIN"),
+                              
+                            ),
+                            # Map Panel
+                            mainPanel(
+                              # Using Shiny JS
+                              #shinyjs::useShinyjs(),
+                              # Style the background and change the page
+                              
+                              # Map Page
+                              leafletOutput("leaflet")
+                            )
+                          )
+                 ),
+                 # Data Table Pannel
+                 tabPanel("Data",
+                          fluidPage(
+                            downloadButton('downloadbutton', 'Download The Covid Data Here!'),
+                            wellPanel(DT::dataTableOutput("table"))
+                          )
+                          
+                 ),
+                 
+                 tabPanel("Lineplot for Emergency Type",
+                          sidebarLayout(
+                            sidebarPanel(
+                              selectInput(
+                                inputId = "Request_Type",
+                                label="Contact approach of the incident",
+                                choices = unique(data$REQUEST_ORIGIN),
+                                selected = "Website"),
+                              
+                            ),
+                            # Map Panel
+                            mainPanel(
+                              plotlyOutput(outputId = "LinePlot")
+                            )
+                          )
+                          
+                 ),
+                 tabPanel("Scatterplot for coordinates",
+                          sidebarLayout(
+                            sidebarPanel(
+                              checkboxGroupInput(
+                                inputId = "weekday",
+                                label="days of week for scatterplot",
+                                choices = unique(data$weekday),
+                                selected = unique(data$weekday)),
+                              
+                            ),
+                            # Map Panel
+                            mainPanel(
+                              plotlyOutput(outputId = "Scatterplot")
+                            )
+                          )
+                          
+                 )
+)
+server <- function(input, output) {
+  
+  output$downloadbutton <- downloadHandler(
+    filename = function() {
+      paste('data-', Sys.Date(), '.csv', sep = '')
+    },
+    content = function(con) {
+      write.csv(data, con)
+    }
+  )
+  
+  dataInputs <- reactive({
+    dataInf <- data 
+    dataInf <- subset(dataInf, weekday %in% input$weekday)
+    return(dataInf)
+  })
+  
+  dataType <- reactive({
+    dataInf <- data_agg 
+    dataInf <- subset(dataInf, REQUEST_ORIGIN == input$Request_Type)
+    return(dataInf)
+  })
+  
+  output$table<-renderDT(data, options = list(lengthChange = FALSE))
+
+  output$Scatterplot<-renderPlotly({
+    data=dataInputs()
+    plot_ly(data = data, x = ~X, y = ~Y,color=~data$weekday)
+    
+  })
+  
+  output$Lineplot<-renderPlotly({
+    data_agg=dataType()
+    plot_ly(data = data_agg, x =~weekday, y = ~N,type="bar")
+    
+  })
+  
+  
+  output$leaflet <- renderLeaflet({
+    
+    
+    
+    leaflet()%>%
+      addProviderTiles("OpenStreetMap.HOT")%>%
+      setView(-80,40.4,zoom = 10)
+  })
+  
+  
+  observe({
+    data=dataInputs()
+    data$Selected <- data[[input$Type]]
+    pal311 <- colorFactor(topo.colors(length(unique(data$Selected))), unique(data$Selected)) 
+    leafletProxy("leaflet", data = data) %>%
+      clearMarkers() %>%
+      clearControls()%>%
+      addCircleMarkers(lng = ~X, lat = ~Y, radius = 1.5, color = ~pal311(Selected), clusterOptions = markerClusterOptions()) %>%
+      addLegend(position = "topright" , pal = pal311, values = data$Selected, title = "REQUEST_ORIGIN")
+  })
+  
+  
+  observe({
+    data=dataInputs()
+    data$Selected <- data[[input$Type]]
+    pal <- colorNumeric(
+      palette = "Purples",
+      domain = police_zone$zone)
+    # Data is greenInf
+    if(input$Type=="POLICE_ZONE"){
+      leafletProxy("leaflet", data = police_zone) %>%
+        clearShapes()%>%
+        addPolygons(color="Purple",popup =~paste(zone))
+    }else{
+      leafletProxy("leaflet", data = police_zone) %>%
+        clearShapes()
+    }
+  })
+}
+
+shinyApp(ui = ui, server = server)
